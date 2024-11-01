1461e1e99e976b633f0055d74ae8f034be76e749
Hui Jun Yang <117896951+aceyang00@users.noreply.github.com>
2024-11-02 03:48:14 +0900

    Update fetch_kamis_data.py

--- fetch_kamis_data.py
+++ fetch_kamis_data.py
@@ -14,14 +14,14 @@ params = {
     'p_cert_key': api_key,                # 발급받은 API 키
     'p_cert_id': 'P_CERT_ID',             # 발급받은 사용자 ID
     'p_returntype': 'xml',                # XML 형식으로 응답 받기
-    'p_startday': '20231103',             # 시작 날짜
-    'p_endday': '20241102',               # 종료 날짜
+    'p_startday': '20240101',             # 시작 날짜
+    'p_endday': '20241231',               # 종료 날짜
     'p_productclscode': '01',             # 소매 가격 조회
     'p_itemcategorycode': '200',          # 예: 과일 부류 코드
-    'p_itemcode': '212',                  # 예: 특정 품목 코드
+    'p_itemcode': '225',                  # 예: 특정 품목 코드
     'p_kindcode': '00',                   # 특정 품종 코드 (없으면 00으로)
-    'p_productrankcode': '04',            # 4등급
-    'p_countrycode': '1101',              # 서울 지역
+    'p_productrankcode': '01',            # 4등급
+    'p_countrycode': '3511',              # 서울 지역
     'p_convert_kg_yn': 'Y'                # kg 단위로 환산
 }
 
